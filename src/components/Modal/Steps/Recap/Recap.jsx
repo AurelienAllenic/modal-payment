39857a761151babad35a273e93125c317d533d28
AurelienAllenic <aurelien.allenic@gmail.com>
2025-11-09 19:56:08 +0100

    (fix) trying to fix link to recap.scss

--- src/components/Modal/Steps/Recap/Recap.jsx
+++ src/components/Modal/Steps/Recap/Recap.jsx
@@ -3,7 +3,7 @@ import "./recap.scss";
 
 const Recap = ({ formData, data }) => {
   console.log(formData, data);
-  return <div>test</div>;
+  return <div className="containerRecap">test</div>;
 };
 
 export default Recap;
